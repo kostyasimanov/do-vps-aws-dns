d32f7c79633669473f8e68a03d9d27b2b5b31ff2
kostyasimanov <kostyasimanov@gmail.com>
2023-03-12 17:21:52 +0300

    Add option to create multiple VPS and records

--- resources.tf
+++ resources.tf
@@ -4,8 +4,9 @@ resource "digitalocean_ssh_key" "personal" {
 }
 
 resource "digitalocean_droplet" "devops_vps" {
+  count    = length(var.domain_name)
   image    = "ubuntu-22-04-x64"
-  name     = "devops.${var.domain_name}"
+  name     = "${element(var.domain_name, count.index)}-${count.index + 1}"
   region   = "fra1"
   size     = "s-1vcpu-1gb"
   tags     = [var.devops_tag, var.current_task_tag, var.email_tag]
@@ -13,9 +14,10 @@ resource "digitalocean_droplet" "devops_vps" {
 }
 
 resource "aws_route53_record" "www" {
+  count   = length(var.domain_name)
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = var.login
+  name    = "${var.login}-${count.index + 1}"
   type    = "A"
   ttl     = 300
-  records = [local.ip_address]
+  records = [element(local.ip_address, count.index)]
 }
